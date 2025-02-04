7e8d235d5077bb16a1e7188b232d21fd59e48b13
Oleh Babenko <olegbabenko12@ukr.net>
2025-02-04 16:32:37 +0200

    nightly run

--- e2e/tests/example.spec.ts
+++ e2e/tests/example.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from '../src/FixtureConfigs';
 
 test.describe('Example test', () => {
 
-    test('Checking the title on the About page', async ({ page, aboutPage }) => {
+    test('@smoke Checking the title on the About page', async ({ page, aboutPage }) => {
         await aboutPage.open();
         await expect(await page.url()).toContain(await aboutPage.getPageUrl());
         await expect(await aboutPage.getTitle()).toContain('Hi, i’m Vadym');
