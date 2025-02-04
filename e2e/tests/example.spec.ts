80cbe27d5b8b8c9b2e8ad8c591ca7868ca5d5103
Oleh Babenko <49614872+OlehBabenkoo@users.noreply.github.com>
2025-02-04 17:34:39 +0200

    VADNAST-22 Integration of PR to Master and Night run (#61)
    * Integration of PR to Master
    
    * Update pr_master.yml
    
    * nightly run
    
    * Update browser for CI/CD
    
    * Update browsers again
    
    * Update night_run.yml
    
    * Update night_run.yml
    
    * Add e2e.yml
    
    * Added report
    
    * Sometimes mistakes happen :-)
    
    * Update night_run.yml

--- e2e/tests/example.spec.ts
+++ e2e/tests/example.spec.ts
@@ -2,7 +2,9 @@ import { expect, test } from '../src/FixtureConfigs';
 
 test.describe('Example test', () => {
 
-    test('Checking the title on the About page', async ({ page, aboutPage }) => {
+    test('Checking the title on the About page', {
+        tag: '@smoke',
+      }, async ({ page, aboutPage }) => {
         await aboutPage.open();
         await expect(await page.url()).toContain(await aboutPage.getPageUrl());
         await expect(await aboutPage.getTitle()).toContain('Hi, i’m Vadym');
