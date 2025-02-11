02b92e73438da8e87df7584deed4865dc70a3126
Maryna Mala <marinamalaa0@gmail.com>
2025-02-11 21:15:21 +0200

    changes made

--- e2e/src/components/Footer.ts
+++ e2e/src/components/Footer.ts
@@ -3,35 +3,56 @@ import BaseComponent from '../base/BaseComponent';
 
 export default class Footer extends BaseComponent {
     private readonly footerLocator = this.page.locator('footer#footer');
-    private readonly linkedInLocator = (this.page.locator('a[href*="linkedin.com"]')).last();
-    private readonly gitHubLocator = (this.page.locator('a[href*="github.com"]')).last();
     private readonly copyrightLocator = (this.page.locator('.copyright'));
 
     constructor(page: Page) {
         super(page);
     }
 
-    get footer(): Locator {
+    static FooterConstants = {
+        COPYRIGHT_TEXT: '© 2025 Vadym Nastoiashchyi',
+    };
+
+    get footerSection(): Locator {
         return this.footerLocator;
     }
 
-    get linkedInLink(): Locator {
-        return this.linkedInLocator;
+    public async allFooterUrls(): Promise<Locator> {
+        return this.page.locator('footer#footer a');
     }
 
-    get gitHubLink(): Locator {
-        return this.gitHubLocator;
+    public async elementsAreVisible(elements: Locator): Promise<void> {
+        for (let i = 0; i < (await elements.count()); i++) {
+            const element = elements.nth(i);
+            await expect(element).toBeVisible();
+        }
     }
 
     get copyright(): Locator {
-        return this.copyrightLocator;
+        return this.footerLocator.locator(this.copyrightLocator);
     }
 
-    public async clickLinkAndVerifyResponse(link: Locator, expectedUrlPart: string): Promise<void> {
-        const [response] = await Promise.all([
-            this.page.waitForResponse(response => response.url().includes(expectedUrlPart) && response.status() === 200),
-            link.click()
-        ]);
-        expect(response.status()).toBe(200);
+    async getAllLinks(footerSection: Locator): Promise<Set<string>> {
+        const links = footerSection.getByRole('link');
+        const allLinks = await links.all();
+        const allhrefs = await Promise.all(
+            allLinks.map((link) => link.getAttribute('href'))
+        );
+        const allValidHrefs = allhrefs.reduce((links, link) => {
+            expect.soft(link).toBeTruthy();
+            if (link) {
+                links.add(new URL(link, this.page.url()).href);
+            }
+            return links;
+        }, new Set<string>());
+        return allValidHrefs;
+    }
+
+    public async verifyLinksResponse(linksUrls: Set<string>): Promise<void> {
+        for(const url of linksUrls) {
+            const response = await this.page.request.get(url);
+            const isSuccessful = (response.status() === 200 || response.status() === 999);
+            expect.soft(isSuccessful).toBeTruthy();
+        }
     }
 }
--- e2e/tests/footer.spec.ts
+++ e2e/tests/footer.spec.ts
@@ -1,24 +1,18 @@
+import Footer from '../src/components/Footer';
 import { test, expect } from '../src/FixtureConfigs';
 
 test.describe('Footer visibility', () => {
     test('Footer section is visible and contains working links', {
         tag: '@regression'
     }, async ({ page, homePage }) => {
-        const github = 'github.com';
-        const linkedin = 'linkedin.com';
-        const copyrightText = '© 2025 Vadym Nastoiashchyi';
-
         await homePage.open();
         expect(page.url()).toContain(await homePage.getPageUrl());
 
-        await expect(homePage.footer.footer).toBeVisible();
+        await expect(homePage.footer.footerSection).toBeVisible();
+        await homePage.footer.elementsAreVisible(await homePage.footer.allFooterUrls());
+        await expect(homePage.footer.copyright).toHaveText(Footer.FooterConstants.COPYRIGHT_TEXT);
 
-        await expect(homePage.footer.linkedInLink).toBeVisible();
-        await expect(homePage.footer.gitHubLink).toBeVisible();
-        await expect(homePage.footer.copyright).toHaveText(copyrightText);
-
-        await homePage.footer.clickLinkAndVerifyResponse(homePage.footer.gitHubLink, github);
-        await homePage.open();
-        await homePage.footer.clickLinkAndVerifyResponse(homePage.footer.linkedInLink, linkedin);
+        const linksUrls = await homePage.footer.getAllLinks(homePage.footer.footerSection);
+        await homePage.footer.verifyLinksResponse(linksUrls);
     });
 });
