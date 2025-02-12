c6cae31feaf5c24d97232a884a5beb032e8ddee0
Oleh Babenko <olegbabenko12@ukr.net>
2025-02-12 13:52:24 +0200

    Update

--- e2e/rpConfig.ts
+++ e2e/rpConfig.ts
@@ -1,16 +1,16 @@
-
+require('dotenv').config();
 
 const rpConfig = {
     endpoint: process.env.REPORT_PORTAL_ENDPOINT,
     apiKey: process.env.REPORT_PORTAL_APIKEY,
     project: process.env.REPORT_PORTAL_PROJECT,
     description: process.env.REPORT_PORTAL_DESCRIPTION,
-    launch: process.env.REPORT_PORTAL_LAUNCH_NAME || 'Default Launch Name',
+    launch: process.env.REPORT_PORTAL_LAUNCH_NAME || "Default Launch Name",
 
     attributes: [
-      { key: 'Type', value: 'Smoke' },
-      { key: 'Type', value: 'Regression' },
-      { key: 'Priority', value: 'High' },
+      { key: "Type", value: "Smoke" },
+      { key: "Type", value: "Regression" },
+      { key: "Priority", value: "High" },
     ],
     mode: 'DEFAULT',
     includeTestSteps: true,
