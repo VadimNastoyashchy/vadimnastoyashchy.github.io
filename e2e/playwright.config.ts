634da8f69aa3381d98423c8f6c67f0538834191c
VadimNastoyashchy <Vadim.89@ukr.net>
2025-08-11 09:16:16 +0300

    Fix playwright config

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -5,27 +5,29 @@ import rpConfig from './rpConfig';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-    testDir: './tests',
-    fullyParallel: true,
-    forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 0,
-    workers: process.env.CI ? 2 : undefined,
-    reporter: [['html'], ['@reportportal/agent-js-playwright', rpConfig]],
-    use: {
-        baseURL: 'https://vadimnastoyashchy.github.io/',
-        trace: 'on-first-retry',
-    },
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 2 : undefined,
+  reporter: process.env.CI
+    ? [['blob'], ['@reportportal/agent-js-playwright', rpConfig]]
+    : 'html',
+  use: {
+    baseURL: 'https://vadimnastoyashchy.github.io/',
+    trace: 'on-first-retry',
+  },
 
-    projects: [
-        {
-            name: 'desktop',
-            use: {
-                ...devices['Desktop Chrome'],
-            },
-        },
-        {
-            name: 'mobile',
-            use: { ...devices['iPhone 13'] },
-        },
-    ],
+  projects: [
+    {
+      name: 'desktop',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+    {
+      name: 'mobile',
+      use: { ...devices['iPhone 13'] },
+    },
+  ],
 });
