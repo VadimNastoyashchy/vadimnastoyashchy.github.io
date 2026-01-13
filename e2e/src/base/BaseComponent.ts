909ac3da8ce906fba2b28f3261ed0bce9a2cfe67
VadimNastoyashchy <Vadim.89@ukr.net>
2026-01-13 11:13:28 +0200

    Remove tests

--- e2e/src/base/BaseComponent.ts
+++
@@ -1,5 +0,0 @@
-import { Page } from '@playwright/test';
-
-export default abstract class BaseComponent {
-  constructor(protected readonly page: Page) {}
-}
--- e2e/src/interfaces/performanceTypes.ts
+++
@@ -1,6 +0,0 @@
-export interface PerfMetrics {
-  stepName: string;
-  timeElapsed: number;
-  visualComplete: number | 'NA';
-  navigationTime: number | 'NA';
-}
