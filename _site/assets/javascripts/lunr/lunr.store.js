84019e6dc677f957a63af9f42debe909f1f88568
VadimNastoyashchy <Vadim.89@ukr.net>
2026-01-14 13:53:45 +0200

    Chanhe site logo width

---
+++ _site/assets/javascripts/lunr/lunr.store.js
@@ -0,0 +1,151 @@
+var store = [{
+        "title": "Automating JavaScript Library Deployment to NPM with GitHub Actions. A Step-by-Step Guide",
+        "excerpt":"This article is useful for test automation engineers or developers who are using JavaScript in their code and want to publish their library to the NPM repository and use it as a dependency in other projects. There are two different ways of publishing. The first is locally using npm commands...","categories": [],
+        "tags": ["JavaScript","Github Actions"],
+        "url": "http://localhost:4000/Automating-js-library"
+      },{
+        "title": "🎭Playwright. How to build Page Object correctly using fixtures",
+        "excerpt":"This article is useful for test automation engineers or developers who are involved in testing and who want to improve their testing framework skills to use Page Object models for test organization, maintenance, and relations between themself. It can also be useful for those engineers who already use this method...","categories": [],
+        "tags": ["JavaScript","Playwright"],
+        "url": "http://localhost:4000/Playwright-how-to-pom"
+      },{
+        "title": "How to automate Accessibility testing using CypressIO?",
+        "excerpt":"This article is useful for test automation engineers or developers who are involved in testing and want to improve their test framework skills to apply such type of testing in the project.Let’s start with a quick overview of accessibility testing itself. ↓↓↓ Digital Accessibility Standards The international standards for coding...","categories": [],
+        "tags": ["JavaScript","Cypress","Accessability"],
+        "url": "http://localhost:4000/accessability-Cypress"
+      },{
+        "title": "🚫STOP using cy.wait()! Use timeout or aliases instead. Cypres IO.",
+        "excerpt":"When writing tests, I often encounter a delay in the appearance of elements on the frontend page. This can be due to waiting for a request with data that the rendering of elements on the page depends on, or other conditions.The easiest way to wait for an item to appear...","categories": [],
+        "tags": ["JavaScript","Cypress"],
+        "url": "http://localhost:4000/Stop-using-cy-wait"
+      },{
+        "title": "CypressIO. How to build Page Object correctly.",
+        "excerpt":"This article is useful for test automation engineers or developers who are involved in testing and want to improve their testing framework skills to use Page Object models to organize testing, maintenance, and communication with each other. It may also be useful for those engineers who are already using this...","categories": [],
+        "tags": ["JavaScript","Cypress","PageObject","Pattern"],
+        "url": "http://localhost:4000/cypress-io-how-to-build-page-object-correctly"
+      },{
+        "title": "Cypress Cloud. Is it worth paying for it?",
+        "excerpt":"We have a set of automated tests that can be run locally on our machine, or they can be integrated as part of continuous integration/continuous deployment. The tests are executed in a single thread, test by test. We use the standard spec reporter to understand and analyze our test results....","categories": [],
+        "tags": ["JavaScript","Cypress","Cypress Cloud"],
+        "url": "http://localhost:4000/cypress-cloud-is-it-worth-paying-for-it"
+      },{
+        "title": "JavaScript 🆚 TypeScript in Test Automation. A Closer Look at Playwright and Cypress",
+        "excerpt":"For any software development project, selecting the right language and tools is crucial to ensure effective test automation. Two languages that have caught the attention of developers worldwide are JavaScript (JS) and TypeScript (TS). Let’s dive into a comparison between these two languages, and specifically how they apply in two...","categories": [],
+        "tags": ["JavaScript","TypeScript","Cypress","Playwright"],
+        "url": "http://localhost:4000/javascript-typescript-in-test-automation-a-closer-look-at-playwright-and-cypress"
+      },{
+        "title": "🤔What is the best code editor for JavaScript?",
+        "excerpt":"JavaScript is one of the world’s most popular programming languages. JavaScript provides integration with CSS and HTML to create robust front-end applications. The introduction of NodeJS has extended the reach of high-level, interpreted programming languages to server-side development. JavaScript also plays an important role in test automation for building test...","categories": [],
+        "tags": ["JavaScript","TypeScript","Webstorm","VSCode"],
+        "url": "http://localhost:4000/what-is-the-best-code-editor-for-javascript"
+      },{
+        "title": "🔝TOP 10 useful Cypress plugins in 2024!",
+        "excerpt":"Cypress is a powerful JavaScript end-to-end testing tool for web applications. As with many other testing tools, Cypress allows for the use of plugins to extend its functionality, making it more robust and flexible to meet the unique demands of different testing applications. Why Extend Cypress With Plugins? Cypress provides...","categories": [],
+        "tags": ["JavaScript","Cypress","Plugins"],
+        "url": "http://localhost:4000/top-10-useful-cypress-plugins-in-2024"
+      },{
+        "title": "😱HOW TO get free parallelization in Cypress IO?",
+        "excerpt":"End-to-end testing is an integral part of any application development pipeline, to simulate real-world user workflows and ensure that they perform as expected. One of the most popular tools for this task is Cypress IO. While Cypress IO offers several benefits, running extensive test suites can be time-consuming. However, you...","categories": [],
+        "tags": ["JavaScript","Cypress","Plugins"],
+        "url": "http://localhost:4000/how-to-get-free-parallelization-in-cypress-io"
+      },{
+        "title": "How do I split one or more large JSON files into a bunch of smaller JSON files?",
+        "excerpt":"Working with JSON (JavaScript Object Notation) is common in web development. JSON has become a popular data exchange format because of its lightweight nature and easy-to-understand syntax. However, we sometimes encounter large JSON files that can be bulky and difficult for other tools to manage, read, or process.In such scenarios,...","categories": [],
+        "tags": ["JavaScript","JSON","Split"],
+        "url": "http://localhost:4000/json-splitter"
+      },{
+        "title": "🔝Top 5 useful Software Testing books",
+        "excerpt":"Software testing is an essential part of any software development life cycle. It ensures a software solution’s functionality, reliability, and efficiency and is vital to quality assurance. Investing time in reading software testing books boosts your understanding of these testing mechanisms, thus ensuring the project’s quality and contributing to your...","categories": [],
+        "tags": ["Book","Testing"],
+        "url": "http://localhost:4000/top-5-useful-software-testing-books"
+      },{
+        "title": "👨‍🏫A beginner’s guide to SOLID principles in JavaScript: Made Simple",
+        "excerpt":"The SOLID Principles are guidelines that help software developers design and maintain clear, robust, and flexible systems. They are essential in object-oriented programming, but they can also be useful in understanding structured coding practices in JavaScript. Here, I’ll break down each principle with simple explanations and examples to make them...","categories": [],
+        "tags": ["SOLID","Patterns","JavaScript"],
+        "url": "http://localhost:4000/a-beginners-guide-to-solid-principles-in-javascript-made-simple"
+      },{
+        "title": "💁‍♂️What is package.json?",
+        "excerpt":"For a long time, I have not abandoned the idea that beginners do not understand certain terms, constructs, and mechanisms of programming language and testing that need a simpler explanation. I want to try to start a series of articles that begin with💁‍♂️What is …. ?I hope the simple examples...","categories": [],
+        "tags": ["Package.json","JavaScript"],
+        "url": "http://localhost:4000/what-is-package-json-in-simple-words-for-beginners"
+      },{
+        "title": "💁‍♂️What is a Testing Piramide?",
+        "excerpt":"In the world of software development, ensuring that an application is bug-free and works as intended is critical. That’s where the “testing pyramid” comes in. The testing pyramid is a concept that helps us understand how to efficiently organize testing efforts to achieve the best results. cdn.britannica.com/06/122506–050-C8E03A8A/Pyramid-… What is the...","categories": [],
+        "tags": ["Testing"],
+        "url": "http://localhost:4000/what-is-a-testing-piramide-in-simple-words"
+      },{
+        "title": "💁‍♂️What is CI/CD Continuous Integration and Continuous Deployment?",
+        "excerpt":"In the fast-paced world of software development, it’s crucial to have systems that allow quick and reliable software updates. This is where CI/CD, or Continuous Integration and Continuous Deployment, comes into play. But what exactly does CI/CD mean, and how does it differ between its components? Let’s break it down...","categories": [],
+        "tags": ["CI/CD","Github Actions","Bamboo","Gitlab","Jenkins"],
+        "url": "http://localhost:4000/what-is-ci-cd-continuous-integration-and-continuous-deployment"
+      },{
+        "title": "cy.get() 🆚 cy.contains() 🆚 cy.find(). The differences. Cypress.",
+        "excerpt":"Cypress IO offers several commands to target and interact with elements, each with specific use cases. Three commonly used commands are: 123456789101112 cy.get(selector) cy.get(alias) cy.get(selector, options) cy.get(alias, options) cy.contains(content) cy.contains(content, options) cy.contains(selector, content) cy.contains(selector, content, options) .find(selector) .find(selector, options)Understanding the differences between these commands can improve how you write tests...","categories": [],
+        "tags": ["Cypress"],
+        "url": "http://localhost:4000/cy-get-cy-contains-cy-find-the-differences-cypress"
+      },{
+        "title": "Test Plan 🆚 Test Strategy. In Simple Words",
+        "excerpt":"In software testing, two essential documents help guide the quality assurance process: the Test Plan and the Test Strategy. While you might hear these terms used interchangeably, they refer to different aspects of the testing process. Let’s simplify these concepts, understand their differences, and explore why they are crucial in...","categories": [],
+        "tags": ["Test Plan","Test Strategy"],
+        "url": "http://localhost:4000/test-plan-test-strategy-in-simple-words"
+      },{
+        "title": "🎭Playwright. Compare the performance of different test parallelization approaches.",
+        "excerpt":"I continue to share tips for playwrights and in the previous article, I showed you how to build a Page Object correctly using fixtures. Now, I shift the focus to test parallelization, which plays a vital role in accelerating test execution using Playwright as a test framework. Time is of...","categories": [],
+        "tags": ["JavaScript","Playwright","Performance"],
+        "url": "http://localhost:4000/playwright-compare-the-performance-of-different-test-parallelization-approaches"
+      },{
+        "title": "🔝JavaScript UI Test Automation Frameworks — which one is faster?",
+        "excerpt":"I continue to share interesting experiences and tips from the automation testing world. So far we’ve discussed the following: Today I have prepared an equally interesting article. As you can see from the title, we are going to compare the performance of three of the most popular system (UI) testing...","categories": [],
+        "tags": ["JavaScript","Playwright","Cypress","WebdriverIO","Performance"],
+        "url": "http://localhost:4000/javascript-ui-test-automation-frameworks-which-one-is-faster"
+      },{
+        "title": "How to Become an Automation QA in JavaScript (From Manual QA)",
+        "excerpt":"Starting a career as an automation quality assurance (AQA) engineer, and moving from a manual QA role, can be both exciting and challenging. This transition involves learning new technical skills, understanding automation frameworks, and implementing test automation best practices. In this article, we’ll go over the basic concepts, tools, and...","categories": [],
+        "tags": ["JavaScript","Automation QA","Manual QA"],
+        "url": "http://localhost:4000/how-to-become-an-automation-qa-in-javascript-from-manual-qa"
+      },{
+        "title": "🔝JavaScript UI Test Automation Frameworks - latest updates in 2025",
+        "excerpt":"Today I have prepared an equally interesting article. Playwright, WebdriverIO, and Cypress are currently the top testing frameworks. They’re always adding new features because of the competition. If you’re an automation or software development engineer, it’s crucial to stay on top of the latest functionalities. This article will give you...","categories": [],
+        "tags": ["JavaScript","Playwright","Cypress","WebdriverIO"],
+        "url": "http://localhost:4000/javascript-ui-test-automation-frameworks-latest-updates"
+      },{
+        "title": "🎭Playwright. Useful video's in 2025",
+        "excerpt":"I’ve been asked for a long time what video courses or lessons I can recommend to colleagues who want to learn or improve their knowledge of Playwright. So in this article I’ve collected some interesting channels that might be useful. JoanMedia Testers Talk Mukesh otwani Currently, these are the top...","categories": [],
+        "tags": ["JavaScript","Playwright"],
+        "url": "http://localhost:4000/playwright-useful-videos-youtube"
+      },{
+        "title": "🔝TOP 5 useful Cypress plugins in 2025!",
+        "excerpt":"I’ve already shared with you 🔝Top 10 useful Cypress plugins in 2024! but not all plugins are included in the article that I use. I want to add 5 more plugins that will be relevant in 2025 and that I use myself. Why Enhance Cypress with Plugins? Despite the growing...","categories": [],
+        "tags": ["JavaScript","Cypress","Plugins"],
+        "url": "http://localhost:4000/top-5-useful-cypress-plugins-in-2025"
+      },{
+        "title": "💁‍♂️for, for…of, .forEach(). What to choose? JavaScript",
+        "excerpt":"We often need to work with different data sets in our daily work. Usually, these sets are stored in an array. JavaScript allows you to go through an array and get your desired values. But it’s important to know the details of the different ways of doing this. Today, we...","categories": [],
+        "tags": ["JavaScript","Cypress","Plugins"],
+        "url": "http://localhost:4000/for-forof-foreach-js-loops"
+      },{
+        "title": "🔝JavaScript UI Test Automation Frameworks - latest updates in 2025 part 2",
+        "excerpt":"Today I have prepared an equally interesting article. Playwright, WebdriverIO, and Cypress are currently the top testing frameworks. They’re always adding new features because of the competition. If you’re an automation or software development engineer, it’s crucial to stay on top of the latest functionalities. This article will give you...","categories": [],
+        "tags": ["JavaScript","Playwright","Cypress","WebdriverIO"],
+        "url": "http://localhost:4000/javascript-ui-test-automation-frameworks-latest-updates-2"
+      },{
+        "title": "ScripterI/O: A Fast and Simple JavaScript ESM Test Runner",
+        "excerpt":"What? ScripterI/O is a JavaScript testing framework designed to run on Node.js. It provides a simple and fast ESM (ECMAScript Module) runner for testing JavaScript code. The framework supports writing test cases using test and describe functions, organizing tests, and performing assertions with the expect function. It includes features like...","categories": [],
+        "tags": ["JavaScript","Scripterio"],
+        "url": "http://localhost:4000/scripterio-js-test-runner"
+      },{
+        "title": "Performing API Testing Using ScripterI/O as a JavaScript Test Runner",
+        "excerpt":"API (Application Programming Interface) testing is an integral part of modern software development processes. It involves testing the interactions between different software components through endpoints and validating their behavior against expected outcomes. Unlike UI testing, API testing focuses exclusively on backend functionality, ensuring data communication and processing are robust, secure,...","categories": [],
+        "tags": ["JavaScript","Scripterio"],
+        "url": "http://localhost:4000/scripterio-js-test-runner-api-tests"
+      },{
+        "title": "🔝JavaScript UI Test Automation Frameworks - latest updates in 2025 part 3",
+        "excerpt":"Today I have prepared an equally interesting article. Playwright, WebdriverIO, and Cypress are currently the top testing frameworks. They’re always adding new features because of the competition. If you’re an automation or software development engineer, it’s crucial to stay on top of the latest functionalities. This article will give you...","categories": [],
+        "tags": ["JavaScript","Playwright","Cypress","WebdriverIO"],
+        "url": "http://localhost:4000/javascript-ui-test-automation-frameworks-latest-updates-3"
+      },{
+        "title": "🎭Playwright Parallelism: Why the Playwright sharding approach speeds up test execution over workers?",
+        "excerpt":"In the previous article, I introduced you to 🎭 Playwright. Compare the performance of different test parallelization approaches. Now, I shift the focus to test parallelization with the sharding approach, which plays a vital role in accelerating test execution using Playwright as a test framework. The topic of parallel test...","categories": [],
+        "tags": ["JavaScript","Playwright","Performance","CI/CD"],
+        "url": "http://localhost:4000/playwright-parallelism-why-the-playwright-sharding-approach-speeds-up-test-execution"
+      }]
